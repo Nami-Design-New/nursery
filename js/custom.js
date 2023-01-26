ca662f05a0bc316546926e3a75272ca73868c12f
mahmoudelkomy996 <mahmoudelkomy996@gmail.com>
2023-01-26 10:01:42 +0200

    start

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  // nav menu
+  $(".navBtn").click(function () {
+    $(this).toggleClass("active");
+    $(".navMenu").toggleClass("active");
+  });
+  //navbar ainmation
+  $(window).scroll(function () {
+    var appScroll = $(document).scrollTop();
+    if (appScroll >= 1) {
+      $("header").addClass("headerAnimate");
+    } else {
+      $("header").removeClass("headerAnimate");
+    }
+  });
+  // modal
+  //////////////////////////////////////////
+
+});
